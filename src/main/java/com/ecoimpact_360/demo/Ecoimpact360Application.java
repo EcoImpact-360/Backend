a62f8e6e3aa974c0cb377dce0ba75a16637e549b
Leonela88 <rivasaela@gmail.com>
2026-02-18 11:36:22 +0100

    firs tommit

---
+++ src/main/java/com/ecoimpact_360/demo/Ecoimpact360Application.java
@@ -0,0 +1,13 @@
+package com.ecoimpact_360.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecoimpact360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecoimpact360Application.class, args);
+	}
+
+}
