107d42ef53fe2f42f385f9d26e8e7b508f428791
ylcruzdev <ylcruz.dev@gmail.com>
2026-02-19 13:42:23 +0100

    resolve conflit pull request

--- src/test/java/com/ecoimpact_360/backend/exception/Ecoimpact360ApplicationTests.java
+++ src/test/java/com/ecoimpact_360/backend/exception/Ecoimpact360ApplicationTests.java
@@ -2,15 +2,12 @@ package com.ecoimpact_360.backend.exception;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-<<<<<<< HEAD:src/test/java/com/ecoimpact_360/backend/exception/Ecoimpact360ApplicationTests.java
 import com.ecoimpact_360.backend.Ecoimpact360Application;
 
 @SpringBootTest(classes = Ecoimpact360Application.class)
 @ActiveProfiles("test")
-=======
-@SpringBootTest
->>>>>>> 137ef4feff82eb52fada9a408fc1f92d6e5dc9c9:src/test/java/com/ecoimpact_360/demo/Ecoimpact360ApplicationTests.java
 class Ecoimpact360ApplicationTests {
 
 	@Test
