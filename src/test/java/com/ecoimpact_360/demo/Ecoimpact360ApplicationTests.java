ae4e47b7681d51b3f55c6e46a680077a90ec6b9b
ylcruzdev <ylcruz.dev@gmail.com>
2026-02-19 11:54:34 +0100

    feat: CorsConfig + GlobalExceptionHandler + ApiErrorDTO + test

--- src/test/java/com/ecoimpact_360/demo/Ecoimpact360ApplicationTests.java
+++ src/test/java/com/ecoimpact_360/demo/Ecoimpact360ApplicationTests.java
@@ -1,9 +1,12 @@
 package com.ecoimpact_360.demo;
 
+import com.ecoimpact_360.backend.Ecoimpact360Application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = Ecoimpact360Application.class)
+@ActiveProfiles("test")
 class Ecoimpact360ApplicationTests {
 
 	@Test
