2222c1fa4e84d3d758c5263a566447a484cd102d
Anna Costa <151736410+annahico@users.noreply.github.com>
2026-02-19 12:49:49 +0100

    Revert "Feature/waste entry controller"

---
+++ src/test/java/com/ecoimpact_360/demo/Ecoimpact360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoimpact_360.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecoimpact360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
